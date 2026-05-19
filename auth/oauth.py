d0e351f47d1237bd9a8d440df789cf6435f9a00f
surapong <Peeraphat27277@gmail.com>
2026-05-19 15:35:48 +0700

    edit : login line 4

--- auth/oauth.py
+++ auth/oauth.py
@@ -20,6 +20,7 @@ oauth.register(
     client_kwargs={
         'scope': 'openid profile email',
         'token_endpoint_auth_method': 'client_secret_post',
+        'id_token_signed_response_alg': 'RS256',
     }
 )
 
