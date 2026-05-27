f825cc0d4269b3ff1feffaafe66e6f592044fade
surapong <Peeraphat27277@gmail.com>
2026-05-27 14:50:46 +0700

    edit : data

--- models/review.py
+++ models/review.py
@@ -19,11 +19,6 @@ class ReviewCreate(BaseModel):
     comment: str = Field(..., min_length=1, max_length=2000)
     images: List[str] = Field(default_factory=list)
 
-    @field_validator("product_id", "user_id")
-    @classmethod
-    def object_id_must_be_valid(cls, value: str) -> str:
-        return validate_object_id(value)
-
 
 class ReviewOut(BaseModel):
     id: str
